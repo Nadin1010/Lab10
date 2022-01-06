89e87ee1019bf995d5bba3b6d33a53f0a876e1a3
Галина <@->
2022-01-06 20:40:22 +0300

    Добавьте файлы проекта.

---
+++ Lab10/Angle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Angle
+    {
+        int gradus;
+        int minute;
+        int second;
+        public bool isCorrect = true;
+
+        public int Gradus
+        {
+            get { return gradus; }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                    gradus = value % 360;
+            }
+        }
+        public int Minute
+        {
+            get { return minute; }
+            set
+            {
+                if (value < 0)
+                    isCorrect = false;
+                else
+                    Gradus += value / 60;
+                minute = value % 60;
+            }
+        }
+        public int Second
+        {
+            get { return second; }
+            set
+            {
+                Minute += value / 60;
+                second = value % 60;
+            }
+        }
+        public Angle(int gradus, int minute, int second)
+        {
+            this.Gradus = gradus;
+            this.Minute = minute;
+            this.Second = second;
+        }
+        public static double ToRadians(double degAngle)
+        { return Math.PI * degAngle / 180; }
+    }
+}
+    
---
+++ Lab10/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Angle angle;
+            do
+            {
+                Console.WriteLine("введите данные");
+                int gradus_ = Convert.ToInt32(Console.ReadLine());
+                int minute_ = Convert.ToInt32(Console.ReadLine());
+                int second_ = Convert.ToInt32(Console.ReadLine());
+
+                angle = new Angle(gradus_, minute_, second_);
+            }
+            while (angle.isCorrect == false);
+
+            int totalSecond = angle.ToRadians();
+            Console.WriteLine(totalSecond);
+            Console.ReadKey();
+        }
+    }
+}
+
+ 
